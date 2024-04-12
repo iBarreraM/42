efeb43bf39c16796b3a8918f0dd51a6fde146428
Inaki Barrera Martin <ibarrera@c3r9s1.42malaga.com>
2024-04-12 16:02:46 +0200

    isdigit

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:13:37 by ibarrera          #+#    #+#             */
-/*   Updated: 2024/04/09 15:44:49 by ibarrera         ###   ########.fr       */
+/*   Updated: 2024/04/12 15:42:31 by ibarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,6 @@ int	ft_isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
-	else return (0);
+	else 
+		return (0);
 }
-
-int	main(void)
-{
-	printf("%i\n",ft_isdigit('1'));
-	printf("%i\n",ft_isdigit('0'));
-	printf("%i\n",ft_isdigit('('));
-
-	printf("%i\n",isdigit('1'));
-	printf("%i\n",isdigit('0'));
-	printf("%i\n",isdigit('('));
-
-	return 0;
-
-}
-
