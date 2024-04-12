8bf753b962a19c0ca8b90f6a8711f91ec6b25ac7
Inaki Barrera Martin <ibarrera@c3r9s1.42malaga.com>
2024-04-12 16:00:48 +0200

    makefile

--- libft/libft.h
+++ libft/libft.h
@@ -6,14 +6,19 @@
 /*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 16:12:31 by ibarrera          #+#    #+#             */
-/*   Updated: 2024/04/11 11:15:02 by ibarrera         ###   ########.fr       */
+/*   Updated: 2024/04/12 15:41:09 by ibarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
+
+#include <ctype.h>
+#include <stdio.h>
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
 
 #endif
