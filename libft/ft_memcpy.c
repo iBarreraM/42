f28dc139ba3b4ccc98804734b31bac3ce654f5c6
Inaki Barrera Martin <ibarrera@c1r7s2.42malaga.com>
2024-04-16 18:35:52 +0200

    memcpy.c

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 12:27:30 by ibarrera          #+#    #+#             */
+/*   Updated: 2024/04/16 18:31:16 by ibarrera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t m;
+	unsigned char *des = (unsigned char *)dst;
+	const unsigned char *sor = (const unsigned char *) src;// por aqui puede ser
+
+	m = 0;
+	if ((src == NULL && dst == NULL))
+		return (dst);
+	while(m < n){
+		des[m] = sor[m];
+			m++;
+	}
+	return (dst);
+}
