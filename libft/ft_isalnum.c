c9596b81543fef25d7e24319f9fe6a7d8ebe6bba
Inaki Barrera Martin <ibarrera@c1r4s2.42malaga.com>
2024-04-16 12:24:46 +0200

    isalnum

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:47:25 by ibarrera          #+#    #+#             */
-/*   Updated: 2024/04/12 15:23:38 by ibarrera         ###   ########.fr       */
+/*   Updated: 2024/04/12 16:04:10 by ibarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	ft_isalnum(int c)
 {
-	if(ft_isalpha(c) == 0 || ft_isdigit(c) == 1)
+	if(ft_isalpha(c) == 1 || ft_isdigit(c) == 1)
 		return (1);
 	else return (0);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 16:12:31 by ibarrera          #+#    #+#             */
-/*   Updated: 2024/04/12 15:41:09 by ibarrera         ###   ########.fr       */
+/*   Updated: 2024/04/16 11:33:38 by ibarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void *ft_memset(void *b, int c, size_t len);
 
 #endif
