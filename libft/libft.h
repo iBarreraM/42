d4b97d75f0014649212ce1c583c6edd548e2dcb5
Inaki Barrera Martin <ibarrera@c1r3s3.42malaga.com>
2024-04-26 13:20:02 +0200

    makefile update

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 16:12:31 by ibarrera          #+#    #+#             */
-/*   Updated: 2024/04/16 18:16:03 by ibarrera         ###   ########.fr       */
+/*   Updated: 2024/04/26 12:42:45 by ibarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # include <ctype.h>
 # include <stdio.h>
 # include <string.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <xlocale.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -26,5 +30,16 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_bzero(void *s, size_t n);
 void 	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
