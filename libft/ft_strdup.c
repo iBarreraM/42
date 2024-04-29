85c24ed10becc004cdc6cf6819094a6dbedddb62
Inaki Barrera Martin <ibarrera@c1r12s4.42malaga.com>
2024-04-29 13:03:53 +0200

    strdup

---
+++ libft/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 12:38:46 by ibarrera          #+#    #+#             */
+/*   Updated: 2024/04/29 12:57:46 by ibarrera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strdup(const char *s1)
+{
+	char *scpy;
+	size_t t;
+	size_t i;
+
+	t = ft_strlen(s1);
+	scpy = (char *)malloc((t + 1) * sizeof(char));
+	if (!scpy)
+		return (NULL);
+
+	i = 0;
+	while (i < t)
+	{
+		scpy[i] = s1[i];
+			i++;
+	}
+	scpy[t] = '\0';
+	return (scpy);
+}
