063f43fe4bc363eefd2f153e4c295a0db3dc85bf
Inaki Barrera Martin <ibarrera@c1r12s4.42malaga.com>
2024-04-29 16:06:22 +0200

    update

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 16:12:31 by ibarrera          #+#    #+#             */
-/*   Updated: 2024/04/29 12:57:18 by ibarrera         ###   ########.fr       */
+/*   Updated: 2024/04/29 15:21:09 by ibarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);void		*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 #endif
