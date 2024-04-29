4b7ca74eea55849598a4111c5b2e49dbaf252f0f
Inaki Barrera Martin <ibarrera@c1r12s4.42malaga.com>
2024-04-29 13:03:32 +0200

    calloc

---
+++ libft/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibarrera <ibarrera@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:17:45 by ibarrera          #+#    #+#             */
+/*   Updated: 2024/04/29 12:21:58 by ibarrera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_calloc(size_t count, size_t size)
+{
+	void *d;
+
+	d = malloc(count * size);
+	if (!d)
+		return (NULL);
+
+	ft_bzero(d, count * size);
+
+	return (d);
+}
